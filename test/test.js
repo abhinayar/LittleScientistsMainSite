a0bcd287dda1202e77bc4bb63249d71c739feea9
Abhi Nayar <abhishek.nayar@yale.edu>
2018-06-05 14:09:54 -0500

    pushing official

--- test/test.js
+++
@@ -1,3 +0,0 @@
-//TODO For future developer: Add unit testing with Mocha and Chai.
-// Out of scope for this project, testing is not included in SOW though internal testing was done with rudimentary 404 and sanity checks.
-
